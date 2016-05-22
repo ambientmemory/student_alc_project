96e370d4cf830a10214b54b0540b2926f47f5534
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2016-05-22 16:24:15 -0400

    trying numpy arrays after lists failed

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-import csv, statistics
-debug = False
-data = []
+import csv, statistics, numpy as np
+debug = True
+data_ext = []
+data_app = []
 no_of_features = 0
+data_loader = []
 
-def load_data():
+'''def load_data():
 	with open('student/student-mat.csv', 'r') as csvfile:
 		data_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
 		next(data_reader)
 		for row in data_reader:
-			no_of_features = len(row)
-			# print(row[23])
-			# data.extend(row[1])
-			# data.append(row)
-			#print(row[3])
-			temp = row[3]
-			print(numerize(row[3]))
+			data_loader = row
+			#print(data_loader)
+			data_ext.extend(data_loader)
+			data_app.append(data_loader)
 #end of load_data
+'''
+
+def load_data():
+	data = np.loadtxt('students/student-mat.csv',
+	                  #dtype={'names':('school','sex','age','address','famsize','pstatus', 'medu', 'fedu', 'mjob', 'fjob', 'reason', 'guardian', 'traveltime', 'studytime','failures','schoolsup','famsup','paid', 'activities', 'nursery', 'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'dalc', 'walc', 'health', 'absences', 'g1', 'g2', 'g3'),
+	                   #      'formats':('S1','S1','i4','S1','S1','S1','i4', 'i4', 'S1', 'S1', 'S1' )},
+	                  delimiter=',',
+	                  usecols=(0,2)
+	                  )
 
 def clean_data():
 	if debug:
-		print(len(data))
-		print(data[:][0]==data[0][:])
-		print([sub_list[0] for sub_list in data])
-		print(data[23])
+		print(data_ext)
+		print("Now printing append")
+		print(data_app)
+		#print(len(data))
+		#print(data[:][0]==data[0][:])
+		#print([sub_list[0] for sub_list in data])
+		#print(data[23])
 	#for i in range(0, no_of_features):
 	#school_name = [sub_list[0] for sub_list in data]
 
