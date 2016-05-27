cc9bfa507aa7d933e978e437a270f2ed185cb9fc
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2016-05-27 11:49:16 -0400

    fixed in pandas and binarization shortcuts

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 import csv, statistics, numpy as np
-debug = True
-data_ext = []
-data_app = []
+import pandas
+
+debug = False
 no_of_features = 0
-data_loader = []
-
-'''def load_data():
-	with open('student/student-mat.csv', 'r') as csvfile:
-		data_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
-		next(data_reader)
-		for row in data_reader:
-			data_loader = row
-			#print(data_loader)
-			data_ext.extend(data_loader)
-			data_app.append(data_loader)
-#end of load_data
-'''
-
-def load_data():
-	data = np.loadtxt('students/student-mat.csv',
-	                  #dtype={'names':('school','sex','age','address','famsize','pstatus', 'medu', 'fedu', 'mjob', 'fjob', 'reason', 'guardian', 'traveltime', 'studytime','failures','schoolsup','famsup','paid', 'activities', 'nursery', 'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'dalc', 'walc', 'health', 'absences', 'g1', 'g2', 'g3'),
-	                   #      'formats':('S1','S1','i4','S1','S1','S1','i4', 'i4', 'S1', 'S1', 'S1' )},
-	                  delimiter=',',
-	                  usecols=(0,2)
-	                  )
 
 def clean_data():
+	data = pandas.read_csv('student/student-mat.csv', sep=';')
+	# making school information redundant
+	data.ix[:, 0] = 0.1
+	'''
+	for i in range(0,395):
+		#binarizing gender: Male = 0, Female = 1
+		if data.ix[i,1]=='M':
+			data.ix[i,1] = 0
+		else:
+			data.ix[i,1] = 1
+		#binarising address: Rural = 0, Urban = 1
+		if data.ix[i, 3] == 'R':
+			data.ix[i, 3] = 0
+		else:
+			data.ix[i, 3] = 1
+		#binarizing famsize: Less than 3 = 0, Greater than 3 = 1
+		if data.ix[i, 4] == 'LE3':
+			data.ix[i, 4] = 0
+		else:
+			data.ix[i, 4] = 1
+	'''
+	# calling numerize
+	data.ix[:,1] = numerize(data.ix[:,1])
+	print(data)
 	if debug:
-		print(data_ext)
-		print("Now printing append")
-		print(data_app)
-		#print(len(data))
-		#print(data[:][0]==data[0][:])
-		#print([sub_list[0] for sub_list in data])
-		#print(data[23])
-	#for i in range(0, no_of_features):
-	#school_name = [sub_list[0] for sub_list in data]
+		print(data)
 
 def numerize(str_list):
-	print(str_list)
 	set_vals = set(str_list)
-	print(set_vals)
+	if debug:
+		print(set_vals)
 	# set_vals contains the A or B options
 	dict_map = {}
 	numerized_list = []
+	#building the index map here
+	i = 0
 	for set_elem in set_vals:
-		for i in range(0,len(set_vals)-1):
-			dict_map = dict(set_elem=i)
-	print(dict_map)
-	for element in str_list:
-		numerized_list = dict_map[element]
-	print(numerized_list)
+		dict_map.update({set_elem:i})
+		i = i+1
+	if debug:
+		print(dict_map)
+
+	for text_thing in str_list:
+		numerized_list.append(dict_map.get(text_thing))
+	if debug:
+		print(numerized_list)
 	return numerized_list
 
 def normalize(numeric_list):
@@ -62,5 +62,4 @@ def normalize(numeric_list):
 
 
 
-load_data()
 clean_data()
